f977649f888e7e5932f4973ffcc3745bca213e61
AliciaT <aliciaju@126.com>
2025-08-03 21:56:30 +0200

    prompt list real-time update

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import PromptPreset
 
 def chat_page(request):
-    return render(request, 'chat/main_chat.html')
+    presets = PromptPreset.objects.all()
+    return render(request, 'chat/main_chat.html', {'presets': presets})
