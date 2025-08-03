309e521ea7b5b3b1d30537c0d3ad8294dad40bc7
AliciaT <aliciaju@126.com>
2025-08-03 23:24:15 +0200

    send to api func

--- chat/urls.py
+++ chat/urls.py
@@ -1,1 +1,1 @@
-from django.urls import pathfrom .views import chat_pageurlpatterns = [    path('', chat_page, name='chat_page'),]
+from django.urls import pathfrom .views import chat_page, save_prompturlpatterns = [    path('', chat_page, name='chat_page'),    path('/prompt/save/',save_prompt, name='save_prompt'),]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from .models import PromptPreset
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .models import PromptPreset
 
 def chat_page(request):
     presets = PromptPreset.objects.all()
     return render(request, 'chat/main_chat.html', {'presets': presets})
+
+@csrf_exempt
+def save_prompt(request):
+    if request.method == 'POST':
+        name = request.POST.get('prompt_option')
+        content = request.POST.get('custom_prompt')
+
+        preset = PromptPreset.objects.filter(name=name).first()
+        if preset:
+            preset.content = content
+            preset.save()
+        return JsonResponse({'status': 'ok'})
